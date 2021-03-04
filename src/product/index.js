2aacdfdc6b2b56a2edad41ab7b13403845fa1fac
Dayom <xiao_d@naver.com>
2021-03-04 22:43:33 +0900

    antd applied

--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,47 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
-  const params = useParams();
   const { id } = useParams();
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+  const [product, setProduct] = useState(null);
+
+  useEffect(function () {
+    axios
+      .get(
+        `https://67e36ce1-98f8-4178-beba-fafc7dd38193.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  console.log(product);
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2020년 12월 8일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
